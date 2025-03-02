976b72e360d8f6efd1c8ef22657f9984e695e377
Adrià Blancafort <adriablancafort@gmail.com>
2025-03-02 02:09:45 +0100

    video demo

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,10 @@
 import { Text, View } from 'react-native';
+import Card from './components/Card';
 
 export default function Home() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl font-bold text-red-800">
-        Hello Sonar
-      </Text>
+      <Card />
     </View>
   );
 }
