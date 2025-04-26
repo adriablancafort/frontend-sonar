1e8c0d2b7f8aff587e7730641bab45850e7b41a6
Adrià Blancafort <adriablancafort@gmail.com>
2025-04-26 20:23:53 +0200

    add route image

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router';
 import { Feather } from '@expo/vector-icons';
@@ -6,12 +6,18 @@ import { Feather } from '@expo/vector-icons';
 export default function StartScreen() {
   return (
     <View className="flex-1 justify-center items-center px-14">
+      <Image 
+        source={require('../assets/images/route.png')}
+        className="w-40 h-40 mb-14"
+        resizeMode="contain"
+      />
+    
       <Text className="text-white text-3xl font-bold mb-6">
         Welcome to MySónar
       </Text>
       
-      <Text className="text-center text-gray-300 text-lg mb-12">
-        Complete this short interactive quiz to get a personalized activity schedule for Sónar 2025 tailored to your preferences!
+      <Text className="text-center text-gray-300 text-lg mb-14">
+        Complete a short interactive quiz to get a personalized schedule for Sónar 2025 tailored to your preferences!
       </Text>
 
       <Link href="/select-schedule" asChild>
