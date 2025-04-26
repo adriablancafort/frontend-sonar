3bc2c4c74a680846ae4a2329932723ffb6065d7c
Adrià Blancafort <adriablancafort@gmail.com>
2025-04-26 19:23:38 +0200

    white status bar icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,17 @@
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
 import '../global.css';
-import { SafeAreaView } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <SafeAreaView>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeAreaView>
+    <>
+      <StatusBar style="light" backgroundColor="black" />
+      <Stack 
+        screenOptions={{ 
+          headerShown: false,
+          contentStyle: { backgroundColor: 'black' } 
+        }}
+      />
+    </>
   );
 }
