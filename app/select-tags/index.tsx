cbce15cdd2c626e25bec3a0dcec5b1e2c40c03aa
Joan <joan.estevan@estudiantat.upc.edu>
2025-05-14 17:06:18 +0200

    Select multiple tags text

--- app/select-tags/index.tsx
+++ app/select-tags/index.tsx
@@ -47,8 +47,11 @@ export default function SelectTags() {
         <ActivityIndicator size="small" color="#ffffff" />
       ) : (
         <>
-          <Text className="text-white text-3xl font-bold mb-12">
+          <Text className="text-white text-3xl font-bold mb-1 mt-12">
             What defines you best?
+          </Text>
+          <Text className="text-white text-3x mb-9">
+            Select as many as you want
           </Text>
           
           <View className="mb-12">
